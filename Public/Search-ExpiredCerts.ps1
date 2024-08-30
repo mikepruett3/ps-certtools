9622658ae458cce337f27afd7df0c21f036dad39
Mike Pruett <mikepruett3@gmail.com>
2024-08-30 11:57:24 -0500

    More code cleanup

--- Public/Search-ExpiredCerts.ps1
+++ Public/Search-ExpiredCerts.ps1
@@ -9,13 +9,14 @@ function Search-ExpiredCerts {
         The remote server to establish a remote PSSession.
     .PARAMETER Credentials
         Credentials used to connect to the remote server, will prompt for password of
-        the associated account. Can be in either DOMAIN\Username or Username@domain.com 
+        the associated account. Can be in either DOMAIN\Username or Username@domain.com
         format.
     .EXAMPLE
         > Search-ExpiredCerts -Server myserver -Credentials mycreds@example.com
     .NOTES
         Author: Mike Pruett
         Date: Feburary 4th, 2022
+        Updated: August 30th, 2024
     #>
     [CmdletBinding()]
     param (
@@ -26,36 +27,35 @@ function Search-ExpiredCerts {
         [string]
         $Credentials
     )
-    
+
     begin {
-        # Import Credentials into $Creds
+        Write-Verbose "Ingesting Credentials of $Credential into variable"
         $Creds = Get-Credential("$Credentials")
 
         # Test Server Connectivity
-        if (!(Test-Connection -Count 1 -ComputerName $Server -Quiet )) {
-            Write-Error "Cannot ping $Server!!!"
-            Break
-        }
+        #if (!(Test-Connection -Count 1 -ComputerName $Server -Quiet )) {
+        #    Write-Error "Cannot ping $Server!!!"
+        #    Break
+        #}
 
-        # Create PSSession Variable - $Session
+        Write-Verbose "Creating PSSession Variable"
         $Session = New-PSSession -ComputerName $Server -Credential $Creds
     }
-    
+
     process {
-        # Query the local Certificate store for Expired Certificates
         # -HideComputerName
+        Write-Verbose "Query the local Certificate store for Expired Certificates"
         Return Invoke-Command -Session $Session -ScriptBlock {
             Return Get-ChildItem -Path Cert:\LocalMachine\My -Recurse -ExpiringInDays 0 | `
                    Select-Object *,@{N="CommonName";E={$_.Subject.Split(',')[0]}}
         } | Select-Object CommonName,FriendlyName,@{N="Expired On";E={$_.NotAfter}},PSComputerName
     }
-    
+
     end {
-        # Remove PSSession
+        Write-Verbose "Removing PSSession Variable"
         Remove-PSSession -Session $Session
 
-        # Cleanup
-        Write-Verbose "Cleaning up used Variables..."
+        Write-Verbose "Cleaning up used Variables"
         Clear-Variable -Name "Server" -ErrorAction SilentlyContinue
         Clear-Variable -Name "Session" -ErrorAction SilentlyContinue
         Clear-Variable -Name "Credentials" -ErrorAction SilentlyContinue
